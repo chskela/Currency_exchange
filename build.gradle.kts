67b30135cfa63e01e70e798561cd8e4338ce43bb
Chistyakov Alexey <chskela@gmail.com>
2024-02-29 17:58:53 +0300

    ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,31 @@
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
 plugins {
     kotlin("jvm") version "1.9.22"
+    id("io.ktor.plugin") version "2.3.8"
 }
 
-group = "com.chskela"
-version = "1.0-SNAPSHOT"
+group = "com.currency_exchange"
+version = "0.0.1"
+
+application {
+    mainClass.set("com.currency_exchange.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
